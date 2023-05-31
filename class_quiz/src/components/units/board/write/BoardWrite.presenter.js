1651d6c30099ea6ad65c65bac6302fba51ce5816
yoonezi <jeji3391@naver.com>
2023-05-31 16:23:39 +0900

    BoardDetailPage -> container/presenter/queries/style.js로 분리

--- class_quiz/src/components/units/board/write/BoardWrite.presenter.js
+++ class_quiz/src/components/units/board/write/BoardWrite.presenter.js
@@ -1,102 +1,75 @@
-// import { Contents, Writer } from "@/styles/boardsDetail";
-
-// import { RadioLabel } from "@/styles/boardsNew";
-// import { ButtonWrapper } from "@/styles/boardsNew";
-import {
-  ButtonWrapper,
-  RadioLabel,
-  Contents,
-  Writer,
-  Title,
-  InputWrapper,
-  Wrapper,
-  WriterWrapper,
-  Label,
-  Error,
-  Subject,
-  ZipcodeWrapper,
-  SearchButton,
-  Address,
-  Youtube,
-  ImageWrapper,
-  UploadButton,
-  OptionWrapper,
-  RadioButton,
-  SubmitButton,
-  Password,
-  Zipcode,
-} from "/styles/boardsNew";
+import * as S from "./BoardWrite.style";
 
 export default function BoardWriteUI(props) {
   return (
-    <Wrapper>
-      <Title>게시글 등록</Title>
-      <WriterWrapper>
-        <InputWrapper>
-          <Label>작성자</Label>
-          <Writer
+    <S.Wrapper>
+      <S.Title>게시글 등록</S.Title>
+      <S.WriterWrapper>
+        <S.InputWrapper>
+          <S.Label>작성자</S.Label>
+          <S.Writer
             type="text"
             placeholder="이름을 적어주세요."
             onChange={props.onChangeWriter}
           />
-          <Error>{props.writerError}</Error>
-        </InputWrapper>
-        <InputWrapper>
-          <Label>비밀번호</Label>
-          <Password
+          <S.Error>{props.writerError}</S.Error>
+        </S.InputWrapper>
+        <S.InputWrapper>
+          <S.Label>비밀번호</S.Label>
+          <S.Password
             type="password"
             placeholder="비밀번호를 작성해주세요."
             onChange={props.onChangePassword}
           />
-          <Error>{props.passwordError}</Error>
-        </InputWrapper>
-      </WriterWrapper>
-      <InputWrapper>
-        <Label>제목</Label>
-        <Subject
+          <S.Error>{props.passwordError}</S.Error>
+        </S.InputWrapper>
+      </S.WriterWrapper>
+      <S.InputWrapper>
+        <S.Label>제목</S.Label>
+        <S.Subject
           type="text"
           placeholder="제목을 작성해주세요."
           onChange={props.onChangeTitle}
         />
-        <Error>{props.titleError}</Error>
-      </InputWrapper>
-      <InputWrapper>
-        <Label>내용</Label>
-        <Contents
+        <S.Error>{props.titleError}</S.Error>
+      </S.InputWrapper>
+      <S.InputWrapper>
+        <S.Label>내용</S.Label>
+        <S.Contents
           placeholder="내용을 작성해주세요."
           onChange={props.onChangeContents}
         />
-        <Error>{props.contentsError}</Error>
-      </InputWrapper>
-      <InputWrapper>
-        <Label>주소</Label>
-        <ZipcodeWrapper>
-          <Zipcode placeholder="07250" />
-          <SearchButton>우편번호 검색</SearchButton>
-        </ZipcodeWrapper>
-        <Address />
-        <Address />
-      </InputWrapper>
-      <InputWrapper>
-        <Label>유튜브</Label>
-        <Youtube placeholder="링크를 복사해주세요." />
-      </InputWrapper>
-      <ImageWrapper>
-        <Label>사진첨부</Label>
-        <UploadButton>+</UploadButton>
-        <UploadButton>+</UploadButton>
-        <UploadButton>+</UploadButton>
-      </ImageWrapper>
-      <OptionWrapper>
-        <Label>메인설정</Label>
-        <RadioButton type="radio" id="youtube" name="radio-button" />
-        <RadioLabel htmlFor="youtube">유튜브</RadioLabel>
-        <RadioButton type="radio" id="image" name="radio-button" />
-        <RadioLabel htmlFor="image">사진</RadioLabel>
-      </OptionWrapper>
-      <ButtonWrapper>
-        <SubmitButton onClick={props.onClickSubmit}>등록하기</SubmitButton>
-      </ButtonWrapper>
-    </Wrapper>
+        <S.Error>{props.contentsError}</S.Error>
+      </S.InputWrapper>
+      <S.InputWrapper>
+        <S.Label>주소</S.Label>
+        <S.ZipcodeWrapper>
+          <S.Zipcode placeholder="07250" />
+          <S.SearchButton>우편번호 검색</S.SearchButton>
+        </S.ZipcodeWrapper>
+        <S.Address />
+        <S.Address />
+      </S.InputWrapper>
+      <S.InputWrapper>
+        <S.Label>유튜브</S.Label>
+        <S.Youtube placeholder="링크를 복사해주세요." />
+      </S.InputWrapper>
+      <S.ImageWrapper>
+        <S.Label>사진첨부</S.Label>
+        <S.UploadButton>+</S.UploadButton>
+        <S.UploadButton>+</S.UploadButton>
+        <S.UploadButton>+</S.UploadButton>
+      </S.ImageWrapper>
+      <S.OptionWrapper>
+        <S.Label>메인설정</S.Label>
+        <S.RadioButton type="radio" id="youtube" name="radio-button" />
+        <S.RadioLabel htmlFor="youtube">유튜브</S.RadioLabel>
+        <S.RadioButton type="radio" id="image" name="radio-button" />
+        <S.RadioLabel htmlFor="image">사진</S.RadioLabel>
+      </S.OptionWrapper>
+      <S.ButtonWrapper>
+        <S.SubmitButton onClick={props.onClickSubmit}>등록하기</S.SubmitButton>
+      </S.ButtonWrapper>
+    </S.Wrapper>
   );
 }
--- class_quiz/src/components/units/detail/BoardDetail.queries.js
+++ class_quiz/src/components/units/detail/BoardDetail.queries.js
@@ -1,1 +1,19 @@
+import { gql } from "@apollo/client";
 
+// boards/new에서 등록한 글 gql-query 로 가져오기 = fetch
+export const FETCH_BOARD = gql`
+  #변수 선언 부분(타입적는곳), fecthBoard는 boardsId라는 변수를 인자로 받고 이 변수는 ID는 타입
+  query fetchBoard($boardId: ID!) {
+    #실제로 쿼리를 실행하는 부분,
+    # boardId는 필드의 인자로 전달되는 값이며, $boardId는 위에서 선언한 변수의 값을 참조합니다.
+    # 이렇게 쿼리 내부에서 변수를 사용하여 값을 전달할 수 있습니다.
+    fetchBoard(boardId: $boardId) {
+      #실제 받을 변수 적는 곳
+      _id
+      writer
+      title
+      contents
+      createdAt
+    }
+  }
+`;
